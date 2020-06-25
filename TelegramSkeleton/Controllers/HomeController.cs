b2f81d3db9833618e35d4d56a5c217e02e2e634c
Vladimir Link <melti-santi@mail.ru>
2020-06-25 17:49:00 +0200

    hello controller

---
+++ TelegramSkeleton/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TelegramSkeleton.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        private static readonly string[] Descriptions = new[]
+        {
+            "Developer", "Optimist", "Dreamer"
+        };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public HomepageMessages Get()
+        {
+            var rng = new Random();
+            return new HomepageMessages
+            {
+                Date = DateTime.Now,
+                Summary = "I am " + Descriptions[rng.Next(Descriptions.Length)] + "!"
+            };
+        }
+    }
+}
---
+++ TelegramSkeleton/HomepageMessages.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TelegramSkeleton
+{
+    public class HomepageMessages
+    {
+        public DateTime Date { get; set; }
+        public string Summary { get; set; }
+    }
+}
